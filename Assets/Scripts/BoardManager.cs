8fb02cd871505ad70d4f55d6360ab2223bcc06ae
Salamander333 <y.glushkov@abv.bg>
2019-06-25 21:12:30 +0300

    etc

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,6 +7,11 @@ public class BoardManager : MonoBehaviour
     public GameObject player_1_model;
     public GameObject player_2_model;
 
+    public int currenPlayerTurn = 1;
+    public int player1_init = 0; 
+    public int player2_init = 0;
+    public bool initFaze = true;
+
     void Start()
     {
         DontDestroyOnLoad(this);
---
+++ Assets/Scripts/RollInitiative.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollInitiative : MonoBehaviour
+{
+    private BoardManager boardManager;
+
+    public Text header;
+
+    void Start()
+    {
+        boardManager = GameObject.Find("_BoardManager").GetComponent<BoardManager>();
+    }
+
+
+}
